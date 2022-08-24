8482141da21d2d9ccdd40d3aa3046155771aab51
Anastasia <sawiii@mail.ru>
2022-08-24 16:05:23 +0300

    First task

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Введите первое число: ");
+//var number1 = Console.ReadLine();
+//Console.WriteLine("Введите второе число: ");
+//var number2 = Console.ReadLine();
+int a1 = 5;
+int a2 = 7;
+int max = a1;
+if(a2 > max ) max = a2;
+Console.WriteLine(max);
